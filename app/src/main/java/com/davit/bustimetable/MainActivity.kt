04a7f61b518765cdd7e590486bd747d17674b20f
david <razvandavidraducanu@bu.educagsd.es>
2024-04-03 14:32:31 +0200

    s

--- app/src/main/java/com/davit/bustimetable/MainActivity.kt
+++ app/src/main/java/com/davit/bustimetable/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +34,6 @@ import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Preview(showSystemUi = true)
 @Composable
 fun App() {
